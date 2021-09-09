9e30fdc89026b201787c8ada7b3cae06c7a2e515
Cássio Rodrigues <79556805+cassiogithub@users.noreply.github.com>
2021-09-09 00:38:01 -0300

    Add files via upload
    comitando

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../templates/Main'
+
+export default props =>
+    <Main icon="home" title="Inicio"
+        subtitle="Segundo projeto">
+        <div className="displa-4">BEM VINDO!</div>
+        <hr />
+        <p className="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nulla distinctio, incidunt repellat pariatur voluptate commodi odio nisi voluptatibus! Repellat doloremque quidem facilis dolores alias pariatur inventore cum ab eum.</p>
+    </Main>
